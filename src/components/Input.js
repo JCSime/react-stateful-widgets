33673f316e399c75a65a015c05f368b0b9233848
John S <johnc.sime@gmail.com>
2021-12-06 17:51:33 -0500

    Got all test to pass on Mood.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -46,7 +46,7 @@ export default function Input() {
     const { value } = evt.target;
 
     /* STEP 4 */
-    setInputValue(evt.target.value)
+    setInputValue(value)
   };
   const reset = () => {
     /* STEP 5 */
