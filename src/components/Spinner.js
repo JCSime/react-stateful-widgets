d7c2152625702f33fc07cbe525804aebc4d2499a
John S <johnc.sime@gmail.com>
2021-12-06 18:27:03 -0500

    Got Spinner.js working but the test do not pass for some reason

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -41,7 +41,7 @@ import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
-const [spinnerOn, setSpinnerOn] = useState(true)
+const [spinnerOn, setSpinnerOn] = useState(true);
 
   const toggleSpinner = () => {
   /* STEP 4 */
@@ -55,7 +55,7 @@ const [spinnerOn, setSpinnerOn] = useState(true)
         spinnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        {spinnerOn ? 'Hide' : 'Show'} {/* STEP 3 */}
+        {spinnerOn ? 'Hide' : 'Show'} Spinner {/* STEP 3 */}
       </button>
     </div>
   );
